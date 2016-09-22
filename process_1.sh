9d650319f03050ddf7cd84d0e48bbc166656f271
Punit Lad <punitlad@gmail.com>
2016-09-22 10:15:58 -0500

    fix for process_1.sh

--- process_1.sh
+++ process_1.sh
@@ -3,5 +3,5 @@
 banner "Hello, SWMS!"
 banner "Goodbye, Dwayne!"
 echo "This line should merge fine."
-echo "I am process_1.sh; I do something."
+echo "I am process_1.sh; I do lots of things."
 echo "This line should merge fine."
