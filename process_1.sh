c7b4062d8486284ddb308775eb83bc28d8694226
Punit Lad <punitlad@gmail.com>
2016-09-22 09:49:45 -0500

    change goodbye to hello

--- process_1.sh
+++ process_1.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 banner "Hello, SWMS!"
-banner "Goodbye, Andrew!"
+banner "Hello, Dwayne!"
 echo "This line should merge fine."
 echo "I am process_1.sh; I do something."
 echo "This line should merge fine."
